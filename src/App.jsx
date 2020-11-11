beabd6cca6061fa8a5445982c522cefc9e5f9869
nipunsingla <piyushsingla2416@gmail.com>
2020-11-11 14:57:28 +0530

    added error page  ,home component and removed redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useContext, useReducer } from "react";
 import Store from "./Store/Store";
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-
-import LoginButton from "./Components/LoginButton";
 import Reducer from "./Store/Reducer";
 import {Home} from './Pages/Home';
 import {About} from './Pages/About';
 import {Contact} from './Pages/Contact';
 import {Devs} from './Pages/Devs';
 import {EventDescription} from './Pages/EventDescription';
+import {HomeComponent} from './Components/HomeComponent';
 import {Events} from './Pages/Events';
 import {Queries} from './Pages/Queries';
 import {Category} from './Pages/Category';
+import { ErrorPage } from "./Pages/Errorpage";
 
 function App() {
   const initialState = useContext(Store);
@@ -19,7 +19,7 @@ function App() {
 
   let routes = (
     <Switch>
-      
+      <Route path='/' exact component={HomeComponent}/>
       <Route
         path="/home"
         exact
@@ -67,19 +67,14 @@ function App() {
         exact
         component={EventDescription}
       />
-<Redirect to="/"/>
-      
+      <Route path="*" component={ErrorPage}/>
     </Switch>
 
   );
   return (
     <Store.Provider value={[state, dispatch]}>
       <div className="App">
-        
-        <h1>bruh workin.</h1>
-        <LoginButton />   
         <Router>
-
         {routes}
         </Router>     
       </div>
