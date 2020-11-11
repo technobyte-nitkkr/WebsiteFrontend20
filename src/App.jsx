0441f57846d835ca2aa5a2abfcbabd465640e5be
nipunsingla <piyushsingla2416@gmail.com>
2020-11-11 13:52:48 +0530

    added home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import { useContext, useReducer } from "react";
 import Store from "./Store/Store";
-import Reducer from "./Store/Reducer";
-
 import LoginButton from "./Components/LoginButton";
+import Reducer from "./Store/Reducer";
+import {Home} from './Pages/Home';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   const initialState = useContext(Store);
   const [state, dispatch] = useReducer(Reducer, initialState);
+
+  let routes = (
+    <Switch>
+      
+      <Route
+        path="/home"
+        exact
+        component={Home}
+      />
+
+    
+<Redirect to="/"/>
+      
+    </Switch>
+
+  );
   return (
     <Store.Provider value={[state, dispatch]}>
       <div className="App">
+        
         <h1>bruh workin.</h1>
-        <LoginButton />
+        <LoginButton />   
+        <Router>
+
+        {routes}
+        </Router>     
       </div>
     </Store.Provider>
   );
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import { React} from 'react';
+import LoginButton from "../Components/LoginButton";
+
+const Home = () => {
+
+
+    return (<div>
+        home page coming soon...
+    </div>)
+}
+
+export {Home};
