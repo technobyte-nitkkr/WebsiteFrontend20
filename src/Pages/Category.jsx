a211827ddf8e930cf3de3157bba33c017e402c73
ashu2305 <ayushgupta2305@gmail.com>
2020-11-11 19:57:58 +0530

    categories fetched

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,7 +1,21 @@
-import { React} from 'react';
+import { React, useState, useEffect} from 'react';
+import axios from 'axios';
+import Key from "../config.keys";
 const Category = () => {
-
-
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        const getEvents = async () => {
+            try {
+                const res = await axios.get( `${Key.BASE_API}/events/categories`);
+                setData({ ...res.data.data });
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getEvents();
+    }, []);
+    console.log(data);
     return (<div>
         Category page coming soon...
     </div>)
