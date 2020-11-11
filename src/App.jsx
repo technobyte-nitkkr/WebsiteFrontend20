3b79d7da1cc30d1536dd4ac3ac131beb1f02175b
saranonearth <saran.suresh@outlook.com>
2020-11-11 11:16:29 +0530

    OAuth implemented | Closes #1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LoginButton from "./Components/LoginButton";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>bruh workin.</h1>
+      <LoginButton />
+    </div>
+  );
+}
+
+export default App;
