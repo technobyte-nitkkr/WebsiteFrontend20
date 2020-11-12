99a417e4d2c27a9f4d89863fd48ec6cacf58bee5
nipunsingla <piyushsingla2416@gmail.com>
2020-11-12 18:29:21 +0530

    user profile api added | closes #11

--- src/Pages/UserProfilePage.jsx
+++ src/Pages/UserProfilePage.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
-const UserProfilePage =()=>{
+import { React, useContext, useEffect } from 'react';
+import Store from '../Store/Store';
+import axios from 'axios';
+import Keys from '../config.keys';
+const UserProfilePage = () => {
+
+    const [userData, setUserData] = useContext(Store);
+    const isAuth = false;
+    useEffect(() => {
+        const getUserDetails = async () => {
+            console.log(userData);
+            try {
+                const url = Keys.BASE_API + '/user';
+                var res = await axios.put(url, {
+                    headers: {
+                        'authorization': userData.token,
+
+                    }
+                });
+                console.log(res);
+
+
+            }
+            catch (e) {
+                console.log(e);
+            }
+        };
+        getUserDetails();
+    });
     return (
         <div>
-            User Profile Page 
+
+            {
+                isAuth ? <div> Yepppe you are logined</div> : <div>You are not logined </div>
+            }
+            User Profile Page
         </div>
     )
 };
-export {UserProfilePage};
+export { UserProfilePage };
