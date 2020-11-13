0928d88896ae14614dfa0c402ba9693cbaad1e46
nipusingla <piyushsingla2416@gmail.com>
2020-11-14 00:16:04 +0530

    changed home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
-import { React } from "react";
+import { React} from "react";
 import { Link } from 'react-router-dom';
 import LoginButton from "../Components/LoginButton";
 const Home = () => {
+
   return (
     <div>
       <div>
