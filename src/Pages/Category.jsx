bf6b65cd55e977e556d6afe4f36707b5b12e8208
insanenerd <thelokeshgoel00@gmail.com>
2020-11-13 17:54:45 +0530

    initial UI added

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -18,10 +18,45 @@ const Category = () => {
         };
         getEvents();
     }, []);
-    console.log(data);
+    console.log(data.categories);
+    var categoriesString=[];
+    // for (var [index,value] of data.categories.entries()){
+    //     var str = "";
+    //     // str=<div className='c-item'> 
+    //     //         <div>
+    //     //             <div className="circle"></div>
+    //     //         </div>
+    //     //         <div> {data.categories[cat]} </div>
+        
+    //     categoriesString.push(<div className='c-item'> <div><div className='circle'></div></div><div> {value} </div>);
+    // }
+    console.log(categoriesString);
+    data.categories.map(cat => console.log(cat))
     return (
     <div>
-        Category page coming soon...
+         <div>
+            <div className="bar">
+                <i className="primary md fa fa-user" aria-hidden="true"></i>
+            </div>
+        </div>
+        <div className="center">
+            <h1 className="main-title">CATEGORIES</h1>
+        </div>
+        <div className="menu">
+            {data.categories.map(cat => (
+                <div>
+                    <div className='m-item'>
+                        <div>
+                            <div className="circle"></div>
+                        </div>
+                        <div>
+                            {cat}
+                        </div>
+                    </div>
+                </div>
+            ) ) }
+        </div> 
+       
     </div>)
 }
 
