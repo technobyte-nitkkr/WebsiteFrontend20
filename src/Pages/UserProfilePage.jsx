c968e302e7c3c9c80e4fda98158ec6f11662e2ec
nipusingla <piyushsingla2416@gmail.com>
2020-11-13 17:24:20 +0530

    added user profile

--- src/Pages/UserProfilePage.jsx
+++ src/Pages/UserProfilePage.jsx
@@ -2,6 +2,7 @@ import { React, useContext, useEffect } from 'react';
 import Store from '../Store/Store';
 import axios from 'axios';
 import Keys from '../config.keys';
+import './UserProfilePage.css';
 const UserProfilePage = () => {
 
     const [userData, setUserData] = useContext(Store);
@@ -17,23 +18,54 @@ const UserProfilePage = () => {
 
                     }
                 });
-                setUserData([...res.data.data]);
-                console.log(res);
+                await setUserData([...res.data.data]);
+                console.log(userData);
             }
             catch (e) {
                 console.log(e);
             }
         };
         getUserDetails();
-    },[]);
+    }, []);
     return (
         <div>
 
-            {
-                userData.isAuth ? <div> Yepppe you are logined</div> : <div>You are not logined </div>
-            }
-            User Profile Page
-        </div>
+            <main className="user-profile">
+                <div className="profile-bg"></div>
+                <section className="user-profile-container">
+                    <aside className="profile-image
+                    ">
+                    </aside>
+                    <section className="profile-info">
+                        <h1 className="first-name">Nipun</h1>
+                        <h1 className="second-name">Gupta</h1>
+
+
+
+                        <h2>Email</h2>
+                        <p>
+                            nipun_11813045@nitkkr.ac.in
+      </p>
+                        <h2>College</h2>
+                        <p>Nit Kurukshetra
+      </p>
+                        <h2>
+                            Year
+
+      </h2>
+      <p>2nd</p>
+
+                </section>
+                </section>
+                <div>
+                    <h1>
+                        Your Events
+                    </h1>
+                    
+                </div>
+            </main>
+
+        </div >
     )
 };
 export { UserProfilePage };
