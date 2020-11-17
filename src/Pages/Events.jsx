d47da49b6246edeba0bc6edc8d3802a2257c95ac
nipusingla <piyushsingla2416@gmail.com>
2020-11-17 18:38:45 +0530

    added event page ui dropdown | closes #23

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -3,12 +3,17 @@ import { format } from 'date-fns';
 import axios from 'axios';
 import './eventPage.css';
 import Key from '../config.keys';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 const Events = props => {
     const category = props.match.params.category;
     const [data, setData] = useState([]);
     const [error, setError] = useState('');
     const [currIndex, setIndex] = useState(0);
+    const [width, setWidth] = useState(window.innerWidth);
+    const [isVisible, setIsVisible] = useState(true);
     useEffect(() => {
+        window.addEventListener("resize", () => setWidth(window.innerWidth));
         window.scrollTo(0, 0);
         const getEvents = async () => {
             try {
@@ -30,70 +35,76 @@ const Events = props => {
             <div className="event-wrapper">
 
                 <header>
-                    <h1>Category Name</h1>
+                    <h1>Category Name
+               {width > 980 ? <span></span> : <span onClick={() => {
+                            setIsVisible(!isVisible);
+                        }} style={{ float: 'right' }}> ☰</span>
+                        }  </h1>
                 </header>
                 <section className="event-columns">
 
+
+                    {!isVisible ? <span></span> :
+                        <div className="event-column">
+                            {
+                                data.map((item, index) =>
+                                    <div key={index} onClick={() => setIndex(index)}>
+
+                                        <h1>{item.eventName} </h1>
+                                        <br />
+                                    </div>
+                                )}
+
+                        </div>}
+
+
                     <div className="event-column">
-                        {
-                            data.map((item, index) =>
-                                <div key={index} onClick={() => setIndex(index)}>
 
-                                    <h1>{item.eventName} </h1>
-                                    <br />
-                                </div>
-                            )}
-
-                    </div>
-
-
-                    <div className="event-column">
-
-                        <h2>{ data[currIndex].eventName}</h2>
+                        <h2>{data[currIndex].eventName}</h2>
                         <p> {data[currIndex].description}</p>
                     </div>
 
                     <div className="event-column">
                         <h2 >
                             <i className="primary md fa fa-calendar" aria-hidden="true"
-                            style={{marginRight:'2rem',}}
+                                style={{ marginRight: '2rem', }}
                             ></i>
 
-                            {format(new Date(data[currIndex].startTime),'dd MMM, yyyy')}
+                            {format(new Date(data[currIndex].startTime), 'dd MMM, yyyy')}
                         </h2>
                         <br></br>
                         <h2 >
                             <i className="primary md fas fa-clock" aria-hidden="true"
-                            style={{marginRight:'2rem',}}
+                                style={{ marginRight: '2rem', }}
                             ></i>
 
-                            {format(new Date(data[currIndex].startTime),'hh:mm aa')} - 
-                            {format(new Date(data[currIndex].endTime),'hh:mm aa')} 
+                            {format(new Date(data[currIndex].startTime), 'hh:mm aa')} -
+                            {format(new Date(data[currIndex].endTime), 'hh:mm aa')}
                         </h2>
-<br/>
+                        <br />
 
                         <h2 >
                             <span
-                            style={{marginRight:'.4rem',}}
+                                style={{ marginRight: '.4rem', }}
                             >Prize - </span>
                             1k
                         </h2>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
                         <h2>Coordinators</h2>
                         <br></br>
-                        {data[currIndex].coordinators.map((item,index)=>
-                         <h2 key={index}>
-                         <span
-                         style={{marginRight:'.4rem',}}
-                         >{item.coordinator_name} - </span>
-                         {item.coordinator_number}
-                     </h2>
-                        
-                        
+                        {data[currIndex].coordinators.map((item, index) =>
+                            <h2 key={index}>
+                                <span
+                                    style={{ marginRight: '.4rem', }}
+                                >{item.coordinator_name} - </span>
+                                {item.coordinator_number}
+                            </h2>
+
+
                         )}
 
-                        <br/>
+                        <br />
                         <div className='event-register-button'>REGISTER</div>
                     </div>
 
