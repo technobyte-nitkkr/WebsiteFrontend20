24bfc679b8da54ff8b21f054ac64c52dc59b37ce
nipusingla <piyushsingla2416@gmail.com>
2020-11-19 12:18:23 +0530

    events page fixed

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -3,7 +3,6 @@ import { format } from 'date-fns';
 import axios from 'axios';
 import './eventPage.css';
 import Key from '../config.keys';
-import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 const Events = props => {
     const category = props.match.params.category;
@@ -20,62 +19,63 @@ const Events = props => {
                 const res = await axios.get(
                     `${Key.BASE_API}/events/description?eventCategory=${category}`,
                 );
-                console.log(res.data.data);
+              //  console.log(res.data.data);
                 setData([...res.data.data.events]);
             } catch (error) {
                 setError(error);
+                
                 console.log(error);
             }
         };
         getEvents();
     }, [category]);
-    console.log(data);
     return (
-        data.length == 0 ? <div className="event-column"></div> :
+        data.length == 0 ? <div ></div> :
             <div style={{ backgroundColour: 'black' }} className="event-wrapper">
 
                 <header>
-                    <h1 style={{
-                        fontWeight:'bold',
-                    }}>Category Name
+                    <h1 className='category-title'>Category Name
                {width > 980 ? <span></span> : <span onClick={() => {
                             setIsVisible(!isVisible);
-                        }} style={{ float: 'right' }}> ☰</span>
+                        }} style={{ float: 'right' }}>
+                            {(isVisible) ? <i className="fa fa-times" aria-hidden="true"></i> :
+                                <span>☰</span>}</span>
                         }  </h1>
                 </header>
                 <section className="event-columns">
 
 
-                    {(isVisible || (width>980)) ? 
-                        <div className="event-column" style={{overflowY:'scroll',height:'100vh'}} >
+
+                    {(isVisible || (width > 980)) ?
+                        <div className="event-column" style={{ overflowY: 'scroll', height: '100vh' }} >
                             {
                                 data.map((item, index) =>
-                                    <div key={index} onClick={() =>{
+                                    <div key={index} onClick={() => {
 
-                                    
-                                     setIndex(index);
-                                     if(width<=980){
-                                         setIsVisible(false);
-                                     }
+
+                                        setIndex(index);
+                                        if (width <= 980) {
+                                            setIsVisible(false);
+                                        }
                                     }
-                                     }>
+                                    }>
 
                                         <h2>
-                                        <i class="fa fa-angle-right" aria-hidden="true"></i> {item.eventName} </h2>
+                                            <i className="fa fa-angle-right" aria-hidden="true"></i> {item.eventName} </h2>
                                         <br />
                                     </div>
                                 )}
 
-                        </div>:<span></span> }
+                        </div> : <span></span>}
 
 
                     <div className="event-column" >
 
                         <h2>{data[currIndex].eventName}</h2>
                         <p style={{
-                            fontSize:'1.5rem'
+                            fontSize: '1.5rem'
                         }}> {data[currIndex].description}</p>
-                        <br/>
+                        <br />
                         <h2 >
                             <i className="primary md fa fa-calendar" aria-hidden="true"
                                 style={{ marginRight: '2rem', }}
