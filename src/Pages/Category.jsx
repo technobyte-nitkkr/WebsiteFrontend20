6301ad5a9bc5fae2cd3336ab3b86b1f78951804f
insanenerd <thelokeshgoel00@gmail.com>
2020-11-19 20:08:51 +0530

    added support for hard coded catgories

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import Key from "../config.keys";
 import './categories.css'
@@ -9,7 +9,7 @@ const Category = () => {
         window.scrollTo(0, 0);
         const getEvents = async () => {
             try {
-                const res = await axios.get( `${Key.BASE_API}/events/categories`);
+                const res = await axios.get(`${Key.BASE_API}/events/categories`);
                 setData({ ...res.data.data });
             } catch (error) {
                 setError(true);
@@ -19,46 +19,47 @@ const Category = () => {
         getEvents();
     }, []);
     console.log(data.categories);
-    var categoriesString=[];
-    // for (var [index,value] of data.categories.entries()){
+    var categoriesString = [];
+    // for (var [index, value] of data.categories.entries()) {
     //     var str = "";
     //     // str=<div className='c-item'> 
     //     //         <div>
     //     //             <div className="circle"></div>
     //     //         </div>
     //     //         <div> {data.categories[cat]} </div>
-        
+
     //     categoriesString.push(<div className='c-item'> <div><div className='circle'></div></div><div> {value} </div>);
     // }
-    data.categories = ['astronomy','astronomy','astronomy','astronomy','astronomy','astronomy','astronomy','astronomy','astronomy',];
+    if (data.categories == undefined)
+        data.categories = ['Astronomy', 'Design', 'astronomy', 'astronomy', 'astronomy', 'astronomy', 'astronomy', 'astronomy', 'astronomy',];
     console.log(categoriesString);
     data.categories.map(cat => console.log(cat))
     return (
-    <div>
-         <div>
-            <div className="bar">
-                <i className="primary md fa fa-user" aria-hidden="true"></i>
+        <div>
+            <div>
+                <div className="bar">
+                    <i className="primary md fa fa-user" aria-hidden="true"></i>
+                </div>
             </div>
-        </div>
-        <div className="center">
-            <h1 className="category-title">CATEGORIES</h1>
-        </div>
-        <div className="menu-categories">
-            {data.categories.map(cat => (
-                <div>
-                    <div className='c-item'>
-                        <div>
-                            <div className="circle"></div>
-                        </div>
-                        <div>
-                            {cat}
+            <div className="center">
+                <h1 className="category-title">CATEGORIES</h1>
+            </div>
+            <div className="menu-categories">
+                {data.categories.map(cat => (
+                    <div>
+                        <div className='c-item'>
+                            <div>
+                                <div className="circle"></div>
+                            </div>
+                            <div>
+                                {cat}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ) ) }
-        </div> 
-       
-    </div>)
+                ))}
+            </div>
+
+        </div>)
 }
 
-export {Category};
+export { Category };
