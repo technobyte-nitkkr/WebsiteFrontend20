40657d7c64753a2d28ff3372ce012dadf9072fee
saranonearth <saran.suresh@outlook.com>
2020-11-21 02:42:40 +0530

    Sponsor typo | /testimonial home route

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { Particle } from "../Components/particle";
 
 const Home = () => {
-
   return (
     <div>
       <div className="particle-style">
@@ -12,36 +11,56 @@ const Home = () => {
         </div>
         <div className="menu">
           <div className="m-item">
-            <Link to='/about'><div className="circle" /></Link>
+            <Link to="/about">
+              <div className="circle" />
+            </Link>
             <div>About</div>
           </div>
           <div className="m-item">
-            <Link to='/categories'><div className="circle" /></Link>
+            <Link to="/categories">
+              <div className="circle" />
+            </Link>
             <div>Category</div>
           </div>
           <div className="m-item">
-            <Link to='/query'><div className="circle" /></Link>
+            <Link to="/query">
+              <div className="circle" />
+            </Link>
             <div>Ask Query</div>
           </div>
           <div className="m-item">
-            <Link to='/contact'><div className="circle" /></Link>
+            <Link to="/contact">
+              <div className="circle" />
+            </Link>
             <div>Contact</div>
           </div>
           <div className="m-item">
-            <Link to='/lectures'><div className="circle" /></Link>
+            <Link to="/lectures">
+              <div className="circle" />
+            </Link>
             <div>Guest Lectures</div>
           </div>
           <div className="m-item">
-            <Link to='/sponsers'><div className="circle" /></Link>
-            <div>Sponsers</div>
+            <Link to="/sponsors">
+              <div className="circle" />
+            </Link>
+            <div>Sponsors</div>
           </div>
           <div className="m-item">
-            <Link to='/devs'><div className="circle" /></Link>
+            <Link to="/testimonial">
+              <div className="circle" />
+            </Link>
+            <div>Testimonials</div>
+          </div>
+          <div className="m-item">
+            <Link to="/devs">
+              <div className="circle" />
+            </Link>
             <div>Devs</div>
           </div>
         </div>
       </div>
-      <Particle/>
+      <Particle />
     </div>
   );
 };
