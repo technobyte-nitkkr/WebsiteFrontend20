3be3773694cf16b58f7baf549d9bd8e6a86fd9a0
nipusingla <piyushsingla2416@gmail.com>
2020-11-23 23:46:44 +0530

    added sponsors

--- src/Pages/UserProfilePage.jsx
+++ src/Pages/UserProfilePage.jsx
@@ -44,6 +44,8 @@ const UserProfilePage = () => {
                 className="profile-image"
                 style={{
                   backgroundImage: `url(https://scontent.fluh1-1.fna.fbcdn.net/v/t1.0-9/s960x960/125764343_3822597857760431_3471134365620395172_o.jpg?_nc_cat=106&ccb=2&_nc_sid=85a577&_nc_ohc=ZWczDiueOh0AX-hxAoO&_nc_ht=scontent.fluh1-1.fna&tp=7&oh=b76f1543e31dc274f037ad2af6ef2a4c&oe=5FDBF09E)`,
+                  backgroundSize:'contain',
+                  backgroundRepeat:'no-repeat'
                 }}
               ></aside>
               <section className="profile-info">
