af672d6a56b96f6a301b09e29456127728ee118a
nipusingla <piyushsingla2416@gmail.com>
2020-11-24 10:32:54 +0530

    added sponsors

--- src/Pages/sponsors.jsx
+++ src/Pages/sponsors.jsx
@@ -3,16 +3,19 @@ import "./sponsors.css";
 import axios from "axios";
 import Keys from "../config.keys";
 import { is } from "date-fns/esm/locale";
+import SponsorList from '../sponsorJsonData.js';
 const Sponsors = () => {
   var arr = ["left", "right", "top", "bottom"];
   var isInWait = false;
   var [sponsorList, setSponsorList] = useState([]);
   useEffect(() => {
+    console.log(SponsorList);
     const getSponsors = async () => {
       try {
         var url = Keys.BASE_API + "/sponsors";
 
         var response = await axios.get(url);
+        console.log(response.data.data.paisa);
 
         setSponsorList(response.data.data.paisa);
       } catch (error) {
@@ -22,38 +25,42 @@ const Sponsors = () => {
     getSponsors();
   }, []);
   return (
-    <div style={{marginBottom:'6rem'}}>
+    <div style={{ marginBottom: '6rem' }}>
       <div className="sponsor-wrapper">
         <h2 className="sponsor-headline typography-sponsor-headline">
           Our Amazing&nbsp;Sponsors
         </h2>
       </div>
       <div className="sponsi-container">
-        {sponsorList.map((item) => {
-          var s = arr[Math.floor(Math.random() * arr.length)];
-          console.log(s);
+        {SponsorList.map((item,index) => {
+          console.log(item.imageUrl);
 
-          return item.sponsors.map((item1, index) => {
+          var s = arr[Math.floor(Math.random() * arr.length)];
+           
+
             return (
               <div key={index} className={`sponsi-card-${s}`}>
                 <div className="sponsi-card-image">
                   <img
                     style={{
-                      objectFit: "fill",
+                      objectFit: "contain",
                     }}
-                    src={item1.imageUrl}
+                    src={item.imageUrl}
                   />
                 </div>
                 <div className="sponsi-card-text">
                   <h2>
-                    <a href={item1.targetUrl}>Visit</a>
+                    <a href={item.targetUrl} style={{
+                      color:'#0EF37F'
+                    }}>{item.name}</a>
                   </h2>
                   <br />
 
                 </div>
-              </div>
+                </div>
+
             );
-          });
+
         })}
       </div>
     </div>
---
+++ src/sponsorJsonData.js
@@ -0,0 +1,29 @@
+module.exports =
+    [
+        {
+            "name": "BuyTheValue",
+            "imageUrl": "https://i.ibb.co/C6LQP8Q/buythevalue.jpg",
+            "targetUrl": "https://buythevalue.in"
+        },
+        {
+            "name": "MergeIntern",
+            "imageUrl": "https://i.ibb.co/tqWqpVn/Merge.png",
+            "targetUrl": "https://www.mergeintern.com"
+        },
+        {
+            "name": "NoticeBard",
+            "imageUrl": "https://i.ibb.co/H2BZmrR/Notice-Bard.jpg",
+            "targetUrl": "https://www.noticebard.com"
+        },
+        {
+            "name": "Quantel",
+            "imageUrl": "https://i.ibb.co/K5R82Rm/quantel.jpg",
+            "targetUrl": "https://quantel.in/consultations"
+        },
+        {
+            "name": "Devfolio",
+            "imageUrl": "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fcc5173fd-2a6a-434e-9d48-1883d77bfc9f%2F_Colored.png?table=block&id=d55db242-49ff-4e05-8fae-865dc9b77873&width=3840&userId=&cache=v2"
+            , "targetUrl": "https://devfolio.co/",
+        }
+
+    ];
